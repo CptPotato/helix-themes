cd255074bd1095769cef5e11c2e0af0c8564887b
CptPotato <3957610+CptPotato@users.noreply.github.com>
2022-01-31 13:21:18 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+process_theme() {
+    for f in "palettes/$1"/*; do
+        local palette="${f##*/}"
+        local target="build/$1_$palette.toml"
+        echo "  $palette -> $target"
+        cat "themes/$1" "$f" > $target
+    done
+}
+
+mkdir -p build
+
+for f in themes/*; do
+    theme="${f##*/}"
+    echo -e "\n$theme:"
+    process_theme "$theme"
+done
